1a5abb665189b077b901b4d541519283db9d1b4e
Fbino <126626726+Fbino@users.noreply.github.com>
2023-03-19 00:41:11 -0300

    proyecto
    aca esta

---
+++ js/funcion.js
@@ -0,0 +1,5 @@
+document.getElementById('boton').onclick = function ( ) {
+    console.log("capturamos el evento click");  
+document.getElementById("boton") .innerHTML= "administrativas y contables,curso realizado online,contancia de la SEC.Año 2015 Windows office 2011:excel,word,punblisher,adobe,corel draw,photodeluxe,etc,quipo de computo,impresoras copiadoras.Dominio de ingles 99%,javascrip año 2015-2022 "
+
+}
